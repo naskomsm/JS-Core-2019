6b7f64a164e6eadc157cbf003715fc19e7a09a91
Atanas Kolev <naskokolev00@gmail.com>
2019-06-23 14:06:26 +0300

    JS-Advanced/05-JS-Classes-EXERCISE/SortedList.js

--- JS-Advanced/05-JS-Classes-EXERCISE/SortedList.js
+++ JS-Advanced/05-JS-Classes-EXERCISE/SortedList.js
@@ -1,27 +1,32 @@
-class List{
-    constructor(){
+class List {
+    constructor() {
         this.array = [];
+        this.size = 0;
     }
 
-    add(number){
+    add(number) {
         this.array.push(number);
+        this.array.sort((a, b) => a - b);
+        this.size++;
     }
 
-    remove(index){
-        this.array.splice(index,1);
+    remove(index) {
+        if (index < this.array.length && index >= 0) {
+            this.array.splice(index, 1);
+            this.size--;
+            this.array.sort((a, b) => a - b);
+        }
     }
 
-    get(index){
-        return this.array[index];
-    }
-
-    get size(){
-        return this.array.length;
+    get(index) {
+        if (index < this.array.length && index >= 0) {
+            return this.array[index];
+        }
     }
 }
 
 let list = new List();
 list.add(5);
-list.add(6);
-list.add(7);
+list.add(3);
+list.remove(0);
 console.log(list.size);
