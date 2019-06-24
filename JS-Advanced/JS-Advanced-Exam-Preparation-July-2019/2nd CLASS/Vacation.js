9df18171c6e3da502e33d7ce6bc5e89d608e08f3
Atanas Kolev <naskokolev00@gmail.com>
2019-06-24 12:33:29 +0300

    JS-Advanced/JS-Advanced-Exam-Preparation-July-2019/

--- JS-Advanced/JS-Advanced-Exam-Preparation-July-2019/2nd CLASS/Vacation.js
+++ JS-Advanced/JS-Advanced-Exam-Preparation-July-2019/2nd CLASS/Vacation.js
@@ -2,105 +2,84 @@ class Vacation {
     constructor(organizer, destination, budget) {
         this.organizer = organizer;
         this.destination = destination;
-        this.budget = budget;
+        this.budget = +budget;
         this.kids = {};
     };
 
     registerChild(name, grade, budget) {
-        if (!this.kids.hasOwnProperty(grade)) {
+        if(budget < this.budget){
+            return `${name}'s money is not enough to go on vacation to ${this.destination}.`
+        }
+
+        if (this.kids.hasOwnProperty(grade)) {
+            for (const string of this.kids[grade]) {
+                if(string === `${name}-${budget}`){
+                    return `${name} is already in the list for this ${this.destination} vacation.`;
+                }
+            }
+
+            this.kids[grade].push(`${name}-${budget}`);
+        }
+
+        else if(!this.kids.hasOwnProperty(grade)){
             this.kids[grade] = [];
+            this.kids[grade].push(`${name}-${budget}`);
         }
-
-        let currentKid = { name: name, budget: budget };
-        if (currentKid.budget < this.budget) {
-            return `${currentKid.name}'s money is not enough to go on vacation to ${this.destination}.`;
-        }
-
-        if (this.kids[grade].some(kid => kid.name === currentKid.name)) {
-            return `${currentKid.name} is already in the list for this ${this.destination} vacation.`
-        }
-
-        this.kids[grade].push(currentKid);
-        //print the current grade
-        let currentGrade = [];
-        this.kids[grade].forEach((kid) => {
-            let stringToPush = `${kid.name}-${kid.budget}`;
-            currentGrade.push(stringToPush);
-        });
-
-        return currentGrade;
+        
+        return this.kids[grade];
     };
 
     removeChild(name, grade) {
-        if (this.kids.hasOwnProperty(grade)) {
-            if (this.kids[grade].some(kid => kid.name === name)) {
-                let kidToRemove = this.kids[grade].find(kid => kid.name === name);
-                let index = this.kids[grade].indexOf(kidToRemove);
-                this.kids[grade].splice(index, 1);
-
-                let currentGrade = [];
-                this.kids[grade].forEach((kid) => {
-                    let stringToPush = `${kid.name}-${kid.budget}`;
-                    currentGrade.push(stringToPush);
-                });
-
-                return currentGrade;
+        if(this.kids.hasOwnProperty(grade)){
+            for (const kid of this.kids[grade]) {
+                let kidInfo = kid.split('-');
+                let kidName = kidInfo[0];
+                if(kidName === name){
+                    let index = this.kids[grade].indexOf(kid);
+                    this.kids[grade].splice(index,1);
+                    return this.kids[grade];
+                }
             }
-            else {
-                return `We couldn't find ${name} in ${grade} grade.`;
-            }
-        }
-        else {
+
             return `We couldn't find ${name} in ${grade} grade.`;
         }
+
+        return `We couldn't find ${name} in ${grade} grade.`;
     }
 
     toString() {
-        if(Object.entries(this.kids).length > 0){
-            const unorderedKids = this.kids;
-            const orderedKids = {};
-            Object.keys(unorderedKids).sort().forEach(function (key) {
-                orderedKids[key] = unorderedKids[key];
-            });
-    
-            const filteredAndOrderedKids = Object.keys(orderedKids).filter(key => unorderedKids[key].length > 0).reduce((obj, key) => {
-                obj[key] = unorderedKids[key];
-                return obj;
-            }, {});
-    
-            let numberOfChildren = 0;
-            for (const key in filteredAndOrderedKids) {
-                numberOfChildren += filteredAndOrderedKids[key].length;
-            }
-            
-            let result = '';
-            result += `${this.organizer} will take ${numberOfChildren} children on trip to ${this.destination}\n`;
-    
-            for (const key in filteredAndOrderedKids) {
-                result += `Grade: ${key}\n`;
-    
-                let index = 1;
-                for (const kid in filteredAndOrderedKids[key]) {
-                   result += `${index++}. ${filteredAndOrderedKids[key][kid].name}-${filteredAndOrderedKids[key][kid].budget}\n`;
-                }
-            }
-    
-            return result;
-        }
-        else{
+        let result = `${this.organizer} will take ${this.numberOfChildren()} children on trip to ${this.destination}\n`;
+        Object.entries(this.kids).sort((a, b) => b[0].localeCompare(a[0]));
+
+
+        if(this.numberOfChildren() === 0){
             return `No children are enrolled for the trip and the organization of ${this.organizer} falls out...`;
         }
-    };
 
-    numberOfChildren(){
-        let numberOfChildren = 0;
-        for (const key in filteredAndOrderedKids) {
-            numberOfChildren += filteredAndOrderedKids[key].length;
+        let currentKidNumber = 1;
+        for (const grade in this.kids) {
+            result += `Grade: ${grade}\n`;
+            currentKidNumber = 1;
+
+            for (const kid in this.kids[grade]) {
+                result += `${currentKidNumber}. ${this.kids[grade][kid]}\n`;
+                currentKidNumber++;
+            }
         }
 
-        return numberOfChildren;
+        return result;
+    };
+
+    numberOfChildren() {
+        return Object.keys(this.kids).length;
     };
 }
 
-let vacation = new Vacation('Miss Elizabeth', 'Dubai', 2000);
+let vacation = new Vacation('Miss. Elizabeth', 'The bahamas', 400);
+
+console.log(vacation.registerChild('Gosho', 12, 3400));
+console.log(vacation.registerChild('Pesho', 12, 400));
+console.log(vacation.registerChild('Pesho', 12, 400));
+console.log(vacation.registerChild('Skaro', 11, 400));
+console.log(vacation.registerChild('Gosho', 11, 3444));
 console.log(vacation.toString());
