5026cefcd6ebb9b1c5050bb1d9aff4f76dc636e2
Atanas Kolev <naskokolev00@gmail.com>
2019-06-30 13:19:42 +0300

    JS-Advanced

--- JS-Advanced/JS-Advanced-Retake-18-April-2019/Organization.js
+++ JS-Advanced/JS-Advanced-Retake-18-April-2019/Organization.js
@@ -4,13 +4,19 @@ class Organization {
         this.name = name;
         this.employees = [];
         this.budget = budget;
+
+        this.deparments = {
+            marketing: 0.4 * this.budget,
+            finance: 0.25 * this.budget,
+            production: 0.35 * this.budget
+        }
     }
 
     get departmentsBudget() {
         return {
-            marketing: 0.4 * this.budget,
-            finance: 0.25 * this.budget,
-            production: 0.35 * this.budget
+            marketing: this.deparments['marketing'],
+            finance: this.deparments['finance'],
+            production: this.deparments['production']
         }
     }
 
@@ -28,58 +34,58 @@ class Organization {
 
         this.employees.push(object);
 
-        this.departmentsBudget[department] -= salary; // problem
+        this.deparments[department] -= salary; // problem
         return `Welcome to the ${department} team Mr./Mrs. ${employeeName}.`
     }
 
     employeeExists(employeeName) {
         let searchedEmployee = this.employees.find(x => x.employeeName === employeeName);
 
-        if(!searchedEmployee){
+        if (!searchedEmployee) {
             return `Mr./Mrs. ${employeeName} is not working in ${this.name}.`;
         }
 
         return `Mr./Mrs. ${searchedEmployee.employeeName} is part of the ${searchedEmployee.department} department.`;
     }
 
-    leaveOrganisation(employeeName){
+    leaveOrganisation(employeeName) {
         let searchedEmployee = this.employees.find(x => x.employeeName === employeeName);
 
-        if(!searchedEmployee){
+        if (!searchedEmployee) {
             return `Mr./Mrs. ${employeeName} is not working in ${this.name}.`;
         }
 
         let index = this.employees.indexOf(searchedEmployee);
-        this.employees.splice(index,1);
-        this.departmentsBudget[searchedEmployee.department] += searchedEmployee.salary; // problem
+        this.employees.splice(index, 1);
+        this.deparments[searchedEmployee.department] += searchedEmployee.salary; // problem
         return `It was pleasure for ${this.name} to work with Mr./Mrs. ${employeeName}.`;
     }
 
-    status(){
+    status() {
         let result = '';
         result += `${this.name.toUpperCase()} DEPARTMENTS:\n`;
 
         //marketing
         let marketingEmployeesNames = [];
-        this.employees.filter(x=>x.department === 'marketing')
-        .sort((a,b) => b.salary - a.salary)
-        .forEach((employee) => marketingEmployeesNames.push(employee.employeeName));
+        this.employees.filter(x => x.department === 'marketing')
+            .sort((a, b) => b.salary - a.salary)
+            .forEach((employee) => marketingEmployeesNames.push(employee.employeeName));
         result += `Marketing | Employees: ${marketingEmployeesNames.length}: ${marketingEmployeesNames.join(', ')} |  Remaining Budget: ${this.departmentsBudget['marketing']}`;
-       
+
         //finance
         let finaceEmployeesNames = [];
-        this.employees.filter(x=>x.department === 'fiance')
-        .sort((a,b) => b.salary - a.salary)
-        .forEach((employee) => finaceEmployeesNames.push(employee.employeeName));
+        this.employees.filter(x => x.department === 'fiance')
+            .sort((a, b) => b.salary - a.salary)
+            .forEach((employee) => finaceEmployeesNames.push(employee.employeeName));
         result += `\nFinance | Employees: ${finaceEmployeesNames.length}: ${finaceEmployeesNames.join(', ')} |  Remaining Budget: ${this.departmentsBudget['finance']}`;
-        
+
         //production
         let productionEmployeesNames = [];
-        this.employees.filter(x=>x.department === 'production')
-        .sort((a,b) => b.salary - a.salary)
-        .forEach((employee) => productionEmployeesNames.push(employee.employeeName));
+        this.employees.filter(x => x.department === 'production')
+            .sort((a, b) => b.salary - a.salary)
+            .forEach((employee) => productionEmployeesNames.push(employee.employeeName));
         result += `\nProduction | Employees: ${productionEmployeesNames.length}: ${productionEmployeesNames.join(', ')} |  Remaining Budget: ${this.departmentsBudget['production']}`;
-        
+
         return result;
     }
 }
