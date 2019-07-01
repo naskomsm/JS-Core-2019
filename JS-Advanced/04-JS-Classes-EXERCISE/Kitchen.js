f44cdbdd6cd706b8d59dab3850ac9b095d94973f
Atanas Kolev <naskokolev00@gmail.com>
2019-07-01 16:07:18 +0300

    JS-Advanced

--- JS-Advanced/04-JS-Classes-EXERCISE/Kitchen.js
+++ JS-Advanced/04-JS-Classes-EXERCISE/Kitchen.js
@@ -1,100 +1,75 @@
-class Kitchen{
-    constructor(budget){
+class Kitchen {
+    constructor(budget) {
         this.budget = budget;
         this.menu = {};
         this.productsInStock = {};
         this.actionsHistory = [];
     }
 
-
-    // working
     loadProducts(products) {
-        for (let product of products) {
-            let productInfo = product.split(' ');
-            let productName = productInfo[0];
-            let productQuantity = +productInfo[1];
-            let productPrice = +productInfo[2];
+        for (const product of products) {
+            let [name, quantity, price] = product.split(' ');
 
-            if (this.budget >= productPrice) {
-                if (!this.productsInStock.hasOwnProperty(productName)) {
-                    this.productsInStock[productName] = 0;
+            if (this.budget >= +price) {
+                if (!this.productsInStock.hasOwnProperty(name)) {
+                    this.productsInStock[name] = 0;
                 }
-
-                this.productsInStock[productName] += productQuantity;
-                this.budget -= productPrice;
-                this.actionsHistory.push(`Successfully loaded ${productQuantity} ${productName}`);
-
-            } else {
-                this.actionsHistory.push(`There was not enough money to load ${productQuantity} ${productName}`);
+                
+                this.productsInStock[name] += +quantity;
+                this.budget -= +price;
+                this.actionsHistory.push(`Successfully loaded ${quantity} ${name}`);
+            }
+            else {
+                this.actionsHistory.push(`There was not enough money to load ${quantity} ${name}`);
             }
         }
 
         return this.actionsHistory.join('\n');
     }
 
-    addToMenu(mealName,products,price){
+    addToMenu(mealName, products, price) {
         if (this.menu.hasOwnProperty(mealName)) {
             return `The ${mealName} is already in our menu, try something different.`;
         }
 
         this.menu[mealName] = {
-            products,
+            products: products,
             price: +price
-        };
+        }
 
         return `Great idea! Now with the ${mealName} we have ${Object.keys(this.menu).length} meals in the menu, other ideas?`;
     }
 
-    showTheMenu(){
-        if(Object.keys(this.menu).length === 0){
+    showTheMenu() {
+        if (Object.keys(this.menu).length === 0) {
             return `Our menu is not ready yet, please come later...`;
         }
 
         let result = '';
         for (const product in this.menu) {
-            result += `${product} - $ ${this.menu[product].price}\n`
+            result += `${product} - $ ${this.menu[product].price}\n`;
         }
 
         return result;
     }
 
-    makeTheOrder(mealName){
+    makeTheOrder(mealName) {
         if (!Object.keys(this.menu).includes(mealName)) {
             return `There is not ${mealName} yet in our menu, do you want to order something else?`;
-        } 
-        else{
-            let haveProductsNeeded = false;
-            for (const productToCheck in this.menu[mealName].products) {
-                let productInfo = this.menu[mealName].products[productToCheck].split(' ');
-                let productToCheckName = productInfo[0];
-                let productToCheckQuantity = +productInfo[1];
+        }
 
-                if(this.productsInStock.hasOwnProperty(productToCheckName) && this.productsInStock[productToCheckName] >= productToCheckQuantity){
-                    haveProductsNeeded = true;
-                }
-                else{
-                    haveProductsNeeded = false;
-                    break;
-                }
+        for (const productToCheck in this.menu[mealName].products) {
+            let [productToCheckName,productToCheckQuantity] = this.menu[mealName].products[productToCheck].split(' ');
+
+            if (this.productsInStock.hasOwnProperty(productToCheckName) && this.productsInStock[productToCheckName] >= +productToCheckQuantity) {
+                this.productsInStock[productToCheckName] -= +productToCheckQuantity;
             }
-
-            //if we dont have the products
-            //if we have it
-            if(haveProductsNeeded){
-                for (const productToCheck in this.menu[mealName].products) {
-                    let productInfo = this.menu[mealName].products[productToCheck].split(' ');
-                    let productToCheckName = productInfo[0];
-                    let productToCheckQuantity = +productInfo[1];
-    
-                    this.productsInStock[productToCheckName] -= productToCheckQuantity;
-                }
-
-                this.budget += this.menu[mealName].price;
-                return `Your order (${mealName}) will be completed in the next 30 minutes and will cost you ${this.menu[mealName].price}.`;
-            }
-            else{
+            else {
                 return `For the time being, we cannot complete your order (${mealName}), we are very sorry...`;
             }
+
         }
+        this.budget += this.menu[mealName].price;
+        return `Your order (${mealName}) will be completed in the next 30 minutes and will cost you ${this.menu[mealName].price}.`;
     }
 }
