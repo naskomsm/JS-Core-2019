c06f724b14c587d2339e8fb49bf467681eb207f4
Atanas Kolev <naskokolev00@gmail.com>
2019-07-02 11:26:21 +0300

    JS-Advanced

--- JS-Advanced/JS-Advanced-Exam-17-March-2019/1st DOM/scripts/solution.js
+++ JS-Advanced/JS-Advanced-Exam-17-March-2019/1st DOM/scripts/solution.js
@@ -14,68 +14,76 @@ function solve() {
       let jsFundamentalsPrice = 170;
       let jsAdvancedPrice = 180;
       let jsApplicationsPrice = 190;
-
       let jsWebPrice = 490;
 
-      if (jsFundamentalsCheckbox.checked) {
-         price += jsFundamentalsPrice;
-         courses.push('JS-Fundamentals');
+      checkedBoxesOperator();
+      onlineFormDiscount();
+      displayTheCourses();
 
-         if (jsAdvancedCheckbox.checked) {
-            jsAdvancedPrice -= jsAdvancedPrice * 0.1;
-            price += jsAdvancedPrice;
-            courses.push('JS-Advanced');
+      function checkedBoxesOperator() {
+         if (jsFundamentalsCheckbox.checked) {
+            price += jsFundamentalsPrice;
+            courses.push('JS-Fundamentals');
 
-            if (jsApplicationsCheckbox.checked) {
-               price += jsApplicationsPrice;
-               price -= price * 0.06;
-               courses.push('JS-Applications');
+            if (jsAdvancedCheckbox.checked) {
+               jsAdvancedPrice -= jsAdvancedPrice * 0.1;
+               price += jsAdvancedPrice;
+               courses.push('JS-Advanced');
 
-               if (jsWebCheckbox.checked) {
-                  price += jsWebPrice;
-                  courses.push('JS-Web');
-                  courses.push('HTML and CSS');
+               if (jsApplicationsCheckbox.checked) {
+                  price += jsApplicationsPrice;
+                  price -= price * 0.06;
+                  courses.push('JS-Applications');
+
+                  if (jsWebCheckbox.checked) {
+                     price += jsWebPrice;
+                     courses.push('JS-Web');
+                     courses.push('HTML and CSS');
+                  }
                }
             }
+            else if (jsApplicationsCheckbox.checked) {
+               price += jsApplicationsPrice;
+               courses.push('JS-Applications');
+            }
+            else if (jsWebCheckbox.checked) {
+               price += jsWebPrice;
+               courses.push('JS-Web');
+            }
          }
-         else if (jsApplicationsCheckbox.checked) {
-            price += jsApplicationsPrice;
-            courses.push('JS-Applications');
-         }
-         else if (jsWebCheckbox.checked) {
-            price += jsWebPrice;
-            courses.push('JS-Web');
+         else {
+            if (jsAdvancedCheckbox.checked) {
+               price += jsAdvancedPrice;
+               courses.push('JS-Advanced');
+            }
+            if (jsApplicationsCheckbox.checked) {
+               price += jsApplicationsPrice;
+               courses.push('JS-Applications');
+            }
+            if (jsWebCheckbox.checked) {
+               price += jsWebPrice;
+               courses.push('JS-Web');
+            }
          }
       }
-      else {
-         if (jsAdvancedCheckbox.checked) {
-            price += jsAdvancedPrice;
-            courses.push('JS-Advanced');
-         }
-         if (jsApplicationsCheckbox.checked) {
-            price += jsApplicationsPrice;
-            courses.push('JS-Applications');
-         }
-         if (jsWebCheckbox.checked) {
-            price += jsWebPrice;
-            courses.push('JS-Web');
+
+      function onlineFormDiscount() {
+         if (document.getElementsByName('educationForm')[1].checked) {
+            price = price - (price * 0.06);
          }
 
+         price = Math.floor(price);
       }
 
-      if (document.getElementsByName('educationForm')[1].checked) {
-         price = price - (price * 0.06);
+      function displayTheCourses() {
+         document.querySelector('.courseFoot p').textContent = `Cost: ${price}.00 BGN`;
+         let ul = document.getElementsByClassName('courseBody')[1].children[0];
+         courses.forEach((course) => {
+            let li = document.createElement('li');
+            li.textContent = course;
+            ul.appendChild(li);
+         });
       }
-
-      price = Math.floor(price);
-
-      document.querySelector('.courseFoot p').textContent = `Cost: ${price}.00 BGN`;
-      let ul = document.getElementsByClassName('courseBody')[1].children[0];
-      courses.forEach((course) => {
-         let li = document.createElement('li');
-         li.textContent = course;
-         ul.appendChild(li);
-      });
    }
 }
 
