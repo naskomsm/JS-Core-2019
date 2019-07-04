dac5a74711ea59965309df7b528f3d74ec00bf46
Atanas Kolev <naskokolev00@gmail.com>
2019-07-04 11:54:56 +0300

    JS-Advanced

--- JS-Advanced/JS-Advanced-Exam-19-December-2018/DartGame - DOM/app.js
+++ JS-Advanced/JS-Advanced-Exam-19-December-2018/DartGame - DOM/app.js
@@ -1,16 +1,21 @@
 function dart() {
+	//setup
 	let layersPointsArray = [];
 	let turns = document.getElementById('turns');
 	let playerInTurn = turns.children[0];
 	let shouldIWork = true;
+	
 	deployScoreboard();
+	attachEventListenersToLayers();
 
-
-	[...document.querySelectorAll('#playBoard div')]
+	
+	function attachEventListenersToLayers(){
+		[...document.querySelectorAll('#playBoard div')]
 		.forEach((layer, index) => {
 			let pointPerLayer = layersPointsArray[index];
 			layer.addEventListener('click', handleScore(pointPerLayer))
 		});
+	}
 
 	function deployScoreboard() {
 		let trs = [...document.querySelectorAll('tbody tr')];
@@ -19,56 +24,61 @@ function dart() {
 			let layerPoints = +tr.children[1].innerHTML.split(' ')[0];
 
 			layersPointsArray.push(layerPoints);
-			layersPointsArray.sort((a, b) => a - b);
 		}
 
 		for (let i = 0; i < trs.length; i++) {
-			trs[i].children[1].innerHTML = layersPointsArray[i]; // layersPoints
+			trs[i].children[1].innerHTML = layersPointsArray[i] + ' points'; // layersPoints
 		}
 	}
 
 	function handleScore(score) {
 		return (ev) => {
 			if (shouldIWork) {
-				if (playerInTurn === turns.children[0]) { // home
-					let homePoints = Number(document.getElementById('Home').children[0].innerHTML);
-					homePoints += score;
-					document.getElementById('Home').children[0].innerHTML = homePoints;
-
-					playerInTurn = turns.children[1];
-
-					turns.children[0].style.textDecoration = 'none';
-					turns.children[0].style.fontWeight = 'normal';
-
-					turns.children[1].style.textDecoration = 'underline';
-					turns.children[1].style.fontWeight = 'bold';
+				swapTurns();
+				function swapTurns(){
+					if (playerInTurn === turns.children[0]) { // home
+						let homePoints = Number(document.getElementById('Home').children[0].innerHTML);
+						homePoints += score;
+						document.getElementById('Home').children[0].innerHTML = homePoints;
+	
+						playerInTurn = turns.children[1];
+	
+						turns.children[0].style.textDecoration = 'none';
+						turns.children[0].style.fontWeight = 'normal';
+	
+						turns.children[1].style.textDecoration = 'underline';
+						turns.children[1].style.fontWeight = 'bold';
+					}
+	
+					else if (playerInTurn === turns.children[1]) {
+						let awayPoints = Number(document.getElementById('Away').children[0].innerHTML);
+						awayPoints += score;
+						document.getElementById('Away').children[0].innerHTML = awayPoints;
+	
+						playerInTurn = document.getElementById('turns').children[0];
+	
+						turns.children[1].style.textDecoration = 'none';
+						turns.children[1].style.fontWeight = 'normal';
+	
+						turns.children[0].style.textDecoration = 'underline';
+						turns.children[0].style.fontWeight = 'bold';
+					}
 				}
 
-				else if (playerInTurn === turns.children[1]) {
-					let awayPoints = Number(document.getElementById('Away').children[0].innerHTML);
-					awayPoints += score;
-					document.getElementById('Away').children[0].innerHTML = awayPoints;
-
-					playerInTurn = document.getElementById('turns').children[0];
-
-					turns.children[1].style.textDecoration = 'none';
-					turns.children[1].style.fontWeight = 'normal';
-
-					turns.children[0].style.textDecoration = 'underline';
-					turns.children[0].style.fontWeight = 'bold';
-				}
-
-				if (Number(document.getElementById('Home').children[0].innerHTML) >= 100) { // home is winner
-					document.getElementById('Home').children[1].style.background = 'green';
-					document.getElementById('Away').children[1].style.background = 'red';
-
-					shouldIWork = false;
-				}
-				else if (Number(document.getElementById('Away').children[0].innerHTML) >= 100) { // away is winner
-					document.getElementById('Home').children[1].style.background = 'red';
-					document.getElementById('Away').children[1].style.background = 'green';
-
-					shouldIWork = false;
+				displayWinner();
+				function displayWinner(){
+					if (Number(document.getElementById('Home').children[0].innerHTML) >= 100) { // home is winner
+						document.getElementById('Home').children[1].style.background = 'green';
+						document.getElementById('Away').children[1].style.background = 'red';
+	
+						shouldIWork = false;
+					}
+					else if (Number(document.getElementById('Away').children[0].innerHTML) >= 100) { // away is winner
+						document.getElementById('Home').children[1].style.background = 'red';
+						document.getElementById('Away').children[1].style.background = 'green';
+	
+						shouldIWork = false;
+					}
 				}
 			}
 
