375a9c1b45cb67171edea7d9555e0ce29552b2b9
Atanas Kolev <naskokolev00@gmail.com>
2019-07-05 12:56:59 +0300

    JS-Advanced

---
+++ JS-Advanced/JS-Advanced-Retake-Exam-30-August-2018/Vacationer-CLASS/Vacationer.js
@@ -0,0 +1,10 @@
+class Vacationer {
+    constructor(firstName, middleName, lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName
+    }
+    //dont know how to overload ctor in js ;(
+
+    //to do
+}
