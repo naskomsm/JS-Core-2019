4c222c5171fd67bb316f7145579cb1bd8f6f76b9
Atanas Kolev <naskokolev00@gmail.com>
2019-07-06 18:25:38 +0300

    JS-Advanced

--- JS-Advanced/JS-Advanced-Exam-15-July-2018/BookCollection - CLASS/BookCollection.js
+++ JS-Advanced/JS-Advanced-Exam-15-July-2018/BookCollection - CLASS/BookCollection.js
@@ -8,5 +8,60 @@ class BookCollection {
         this.room = room;
         this.shelfGenre = shelfGenre;
         this.shelfCapacity = shelfCapacity;
-}
+    }
+
+    get shelfCondition() {
+        return this.shelfCapacity - this.shelf.length
+    }
+
+    addBook(bookName, bookAuthor, genre = '') {
+        let book = {
+            bookName,
+            bookAuthor,
+            genre: genre
+        }
+
+        if (this.shelf.length === this.shelfCapacity) {
+            this.shelf.shift();
+            this.shelf.unshift(book);
+        }
+        else {
+            this.shelf.push(book);
+        }
+
+        this.shelf.sort((a, b) => a.bookAuthor.localeCompare(b.bookAuthor));
+        return this;
+    }
+
+    throwAwayBook(bookName) {
+        this.shelf = this.shelf.filter(b => b.bookName !== bookName);
+    }
+
+    showBooks(genre) {
+        let searchedBooks = this.shelf.filter(x => x.genre === genre);
+
+        let info = [];
+        info.push(`Results for search "${genre}":`);
+
+        for (const book of searchedBooks) {
+            info.push(`\uD83D\uDCD6 ${book.bookAuthor} - "${book.bookName}"`)
+        }
+
+        return info.join('\n');
+    }
+
+    toString() {
+        if (this.shelf.length === 0) {
+            return 'It\'s an empty shelf';
+        }
+
+        let info = [];
+        info.push(`"${this.shelfGenre}" shelf in ${this.room} contains:`);
+
+        for (const book of this.shelf) {
+            info.push(`\uD83D\uDCD6 "${book.bookName}" - ${book.bookAuthor}`)
+        }
+
+        return info.join('\n');
+    }
 }
