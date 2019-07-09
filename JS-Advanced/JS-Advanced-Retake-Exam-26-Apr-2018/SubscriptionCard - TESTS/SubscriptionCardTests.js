3069179e9e1d6c631595f3ba17a671dbca23c0ed
Atanas Kolev <naskokolev00@gmail.com>
2019-07-09 13:05:33 +0300

    JS-Advanced

--- JS-Advanced/JS-Advanced-Retake-Exam-26-Apr-2018/SubscriptionCard - TESTS/SubscriptionCardTests.js
+++ JS-Advanced/JS-Advanced-Retake-Exam-26-Apr-2018/SubscriptionCard - TESTS/SubscriptionCardTests.js
@@ -1,3 +1,80 @@
 let assert = require("chai").assert;
 let SubscriptionCard = require('./SubscriptionCard');
 
+describe('constructor and getters',function(){
+    it('init should work - getters and ctor',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        assert.equal(current.firstName,'Atanas');
+        assert.equal(current.lastName,'Kolev');
+        assert.equal(current.SSN,'00000000');
+        assert.equal(current.isBlocked,false);
+        assert.deepEqual(current._subscriptions,[]);
+    });
+}); 
+
+describe('addSubscription',function(){
+    it('working with some line',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        current.addSubscription('120', new Date('2018-04-22'), new Date('2018-05-21'));
+        let dummy = {
+            endDate: new Date('2018-05-21'),
+            line: '120',
+            startDate: new Date('2018-04-22')
+        };
+        assert.deepEqual(current._subscriptions,[dummy]);
+    }); 
+
+    it('working with *',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        current.addSubscription('*', new Date('2018-04-22'), new Date('2018-05-21'));
+        let dummy = {
+            endDate: new Date('2018-05-21'),
+            line: '*',
+            startDate: new Date('2018-04-22')
+        };
+        assert.deepEqual(current._subscriptions,[dummy]);
+    });
+});
+
+describe('isValid',function(){
+    it('unvalid',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        assert.equal(false,current.isValid('120', new Date('2018-04-22')));
+    });
+
+    it('blocked',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        current.addSubscription('120', new Date('2018-04-22'), new Date('2018-05-21'));
+        current.block();
+        assert.equal(false,current.isValid('120', new Date('2018-04-22')));
+    });
+
+    it('valid',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        current.addSubscription('120', new Date('2018-04-22'), new Date('2018-05-21'));
+        assert.equal(true,current.isValid('120', new Date('2018-04-22')));
+    });
+
+    it('valid',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        current.addSubscription('*', new Date('2018-04-22'), new Date('2018-05-21'));
+        assert.equal(true,current.isValid('*', new Date('2018-04-22')));
+    });
+});
+
+describe('block and unblock',function(){
+    it('block',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        current.addSubscription('120', new Date('2018-04-22'), new Date('2018-05-21'));
+        current.block();
+        assert.equal(true,current.isBlocked);
+    });
+
+    it('unblock',function(){
+        let current = new SubscriptionCard('Atanas','Kolev','00000000');
+        current.addSubscription('120', new Date('2018-04-22'), new Date('2018-05-21'));
+        current.block();
+        current.unblock();
+        assert.equal(false,current.isBlocked);
+    });
+}); 
