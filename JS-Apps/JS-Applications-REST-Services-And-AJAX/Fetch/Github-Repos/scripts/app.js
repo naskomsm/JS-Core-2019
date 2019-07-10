9237705f2d2cdd1d16b6fb3efd8b7f1b56943b47
Atanas Kolev <naskokolev00@gmail.com>
2019-07-10 12:13:21 +0300

    JS-Apps

--- JS-Apps/JS-Applications-REST-Services-And-AJAX/Fetch/Github-Repos/scripts/app.js
+++ JS-Apps/JS-Applications-REST-Services-And-AJAX/Fetch/Github-Repos/scripts/app.js
@@ -1,3 +1,29 @@
 function loadRepos() {
-	console.log("TODO...");
+	let username = document.getElementById('username').value;
+	let reposList = document.getElementById('repos');
+
+	function parseRepo({ html_url, full_name }) {
+		return { link: html_url, name: full_name };
+	}
+
+	function toDomElement({ name, link }) {
+		let listItem = document.createElement('li');
+		let linkItem = document.createElement('a');
+
+		linkItem.href = link;
+		linkItem.innerHTML = name;
+		listItem.appendChild(linkItem);
+		return listItem;
+	}
+
+	fetch(`https://api.github.com/users/${username}/repos`)
+		.then(response => response.json())
+		.then(repos =>
+			repos.map(parseRepo)
+				.map(toDomElement)
+				.forEach(el => {
+					reposList.appendChild(el);
+				}))
+		.catch((error) => console.error(error))
+
 }
