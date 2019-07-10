40764c2a18ca60e78c48a0609fce4b00e994c700
Atanas Kolev <naskokolev00@gmail.com>
2019-07-10 17:30:00 +0300

    JS-Apps

--- JS-Apps/JS-Applications-REST-Services-And-AJAX - EXERCISE/02.BUS-SCHEDULE/app.js
+++ JS-Apps/JS-Applications-REST-Services-And-AJAX - EXERCISE/02.BUS-SCHEDULE/app.js
@@ -9,27 +9,40 @@ function solve() {
     function depart() {
         function display(busStop) {
             const { name, next } = busStop;
-            
+
             currentStop.name = name;
             currentStop.next = next;
 
             info.textContent = `Next stop ${currentStop.name}`;
 
-            document.getElementById('depart').disabled = true;   
-            document.getElementById('arrive').disabled = false;   
+            document.getElementById('depart').disabled = true;
+            document.getElementById('arrive').disabled = false;
         }
 
         let url = `https://judgetests.firebaseio.com/schedule/${currentStop.next}.json `;
         fetch(url)
-            .then((response) => response.json())
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                else{
+                    displayError();
+                }
+            })
             .then((data) => display(data))
     }
 
     function arrive() {
         info.textContent = `Arriving at ${currentStop.name}`;
 
-        document.getElementById('depart').disabled = false;   
+        document.getElementById('depart').disabled = false;
         document.getElementById('arrive').disabled = true;
+    }
+
+    function displayError() {
+        const stopInfo = document.getElementsByClassName('info')[0];
+        stopInfo.textContent = `Error`;
+        disableButtons();
     }
 
     return {
