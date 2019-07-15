6af15489c49e8b8a4e41ed2af4f6620e258e7cf3
Atanas Kolev <naskokolev00@gmail.com>
2019-07-15 18:53:15 +0300

    JS-Apps

--- JS-Apps/JS-Applications-REST-Services-And-AJAX - EXERCISE/03.PHONEBOOK/app.js
+++ JS-Apps/JS-Applications-REST-Services-And-AJAX - EXERCISE/03.PHONEBOOK/app.js
@@ -2,12 +2,16 @@ function attachEvents() {
     const elements = {
         loadButton: document.getElementById('btnLoad'),
         createButton: document.getElementById('btnCreate'),
-        phonebook: document.getElementById('phonebook')
+        phonebook: document.getElementById('phonebook'),
+        personName: document.getElementById('person'),
+        personPhone: document.getElementById('phone')
     };
 
+    elements.createButton.addEventListener('click', createContact);
     elements.loadButton.addEventListener('click', loadContacts);
 
     function loadContacts() {
+        elements.phonebook.innerHTML = '';
         fetch(`https://phonebook-nakov.firebaseio.com/phonebook.json`)
             .then(handler)
             .then(showContacts);
@@ -17,32 +21,49 @@ function attachEvents() {
         for (const contact in data) {
             const id = contact;
             const { person, phone } = data[contact];
-            
-            const li = createLi(person, phone,id);
+
+            const li = createLi(person, phone, id);
             elements.phonebook.appendChild(li);
         }
     }
-    
-    function createLi(person, phone,id) {
-        const deleteButton = createDeleteButton();
+
+    function createLi(person, phone, id) {
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'DELETE';
+        deleteButton.addEventListener('click', deleteContact);
+
         let li = document.createElement('li');
         li.textContent = `${person}: ${phone}`;
         li.appendChild(deleteButton);
 
+        function deleteContact() {
+            const contactToRemove = this.parentNode;
+
+            fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${id}.json`, {
+                method: 'DELETE'
+            })
+                .then(() => contactToRemove.remove())
+                .then(loadContacts);
+        }
+
         return li;
     }
 
-    function createDeleteButton() {
-        let button = document.createElement('button');
-        button.textContent = 'DELETE';
-        button.addEventListener('click',deleteContact);
+    function createContact() {
+        const contact = {
+            'person': elements.personName.value,
+            'phone': elements.personPhone.value
+        };
 
-        return button;
+        elements.personName.value = '';
+        elements.personPhone.value = '';
+
+        fetch(`https://phonebook-nakov.firebaseio.com/phonebook.json`, {
+            method: 'POST',
+            body: JSON.stringify(contact)
+        }).then(loadContacts);
     }
 
-    function deleteContact(id) {
-
-    }
 
     function handler(response) {
         if (response.status > 400) {
