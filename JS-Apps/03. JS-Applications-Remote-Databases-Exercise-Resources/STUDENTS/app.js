6769bd56a000cf3bcbdc1c57f79cf98b6c2460ab
Atanas Kolev <naskokolev00@gmail.com>
2019-07-18 19:34:59 +0300

    JS-Apps

--- JS-Apps/03. JS-Applications-Remote-Databases-Exercise-Resources/STUDENTS/app.js
+++ JS-Apps/03. JS-Applications-Remote-Databases-Exercise-Resources/STUDENTS/app.js
@@ -4,9 +4,17 @@ function students() {
         loadButton: document.getElementsByClassName('load')[0],
         addButton: document.getElementsByClassName('add')[0],
         baseURL: `https://baas.kinvey.com/appdata/kid_HJ6_eU2bH`,
-        tableBody: document.getElementsByTagName('tbody')[0]
+        tableBody: document.getElementsByTagName('tbody')[0],
+        id: document.getElementsByClassName('id')[0],
+        firstName: document.getElementsByClassName('firstName')[0],
+        lastName: document.getElementsByClassName('lastName')[0],
+        facultyNumber: document.getElementsByClassName('facultyNumber')[0],
+        grade: document.getElementsByClassName('grade')[0],
     }
 
+    let allStudents = [];
+
+    elements.addButton.addEventListener('click', addStudent);
     elements.loadButton.addEventListener('click', loadStudents);
 
     let headers = new Headers();
@@ -17,7 +25,7 @@ function students() {
         const loadURL = elements.baseURL + `/students`;
         elements.tableBody.innerHTML = 'Loading...';
 
-        fetch(loadURL,{
+        fetch(loadURL, {
             method: 'get',
             headers: headers
         })
@@ -25,20 +33,58 @@ function students() {
             .then(showStudents);
     }
 
-    function showStudents(students){
+    function showStudents(students) {
         elements.tableBody.innerHTML = '';
+        allStudents = [];
+
         students.forEach(student => {
+            const myObj = {
+                'Id': student.Id,
+                'FirstName': student.FirstName,
+                'LastName': student.LastName,
+                'FacultyNumber': student.FacultyNumber,
+                'Grade': student.Grade
+            }
+
+            allStudents.push(myObj);
+        });
+
+        allStudents.sort((a,b) => a.Id - b.Id).forEach(student => {
             const tr = createHTMLelement('tr');
+            
+            const Idtd = createHTMLelement('td', student.Id);
+            const firstNameTd = createHTMLelement('td', student.FirstName);
+            const lastNameTd = createHTMLelement('td', student.LastName);
+            const facultyNumberTd = createHTMLelement('td', student.FacultyNumber);
+            const gradeTd = createHTMLelement('td', student.Grade);
 
-            const Idtd = createHTMLelement('td',student.Id);
-            const firstNameTd = createHTMLelement('td',student.FirstName);
-            const lastNameTd = createHTMLelement('td',student.LastName);
-            const facultyNumberTd = createHTMLelement('td',student.FacultyNumber);
-            const gradeTd = createHTMLelement('td',student.Grade);
-
-            appendChildren(tr,[Idtd,firstNameTd,lastNameTd,facultyNumberTd,gradeTd]);
+            appendChildren(tr, [Idtd, firstNameTd, lastNameTd, facultyNumberTd, gradeTd]);
             elements.tableBody.appendChild(tr);
         });
+    }
+
+    function addStudent() {
+        const addURL = elements.baseURL + `/students`;
+
+        if (elements.id.value && elements.firstName.value && elements.lastName.value && elements.facultyNumber.value && elements.grade.value) {
+            const myObj = {
+                'Id': elements.id.value,
+                'FirstName': elements.firstName.value,
+                'LastName': elements.lastName.value,
+                'FacultyNumber': elements.facultyNumber.value,
+                'Grade': elements.grade.value
+            }
+
+            allStudents.push(myObj);
+
+            fetch(addURL, {
+                method: 'post',
+                headers:headers,
+                body: JSON.stringify(myObj),
+            }).then(loadStudents);
+
+            clear();
+        }
     }
 
     function appendChildren(parent, children) {
@@ -62,7 +108,11 @@ function students() {
     }
 
     function clear() {
-
+        elements.id.value = '';
+        elements.firstName.value = '';
+        elements.lastName.value = '';
+        elements.facultyNumber.value = '';
+        elements.grade.value = '';
     }
 
     function handler(response) {
